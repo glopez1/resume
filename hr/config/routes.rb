213e831a7eb7bedbd62d30af074c522093e5dea6
EC2 Default User <ec2-user@ip-172-31-54-183.ec2.internal>
2022-05-16 14:33:15 +0000

    rails installed

--- hr/config/routes.rb
+++ hr/config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
- 
-  resources :resumes do
-    resources :skills
-  end
-  
-  root 'welcome#index'
-  
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
